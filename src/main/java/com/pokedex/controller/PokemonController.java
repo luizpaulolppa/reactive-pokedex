c3e3a9695ddcafc050958b159b3a6a97813e9b6a
Luiz Paulo Pilegi de Almeida <luizpilegilppa@gmail.com>
2020-07-26 01:22:38 -0300

    Criado novos métodos de remoção dos pokemons

--- src/main/java/com/pokedex/controller/PokemonController.java
+++ src/main/java/com/pokedex/controller/PokemonController.java
@@ -2,9 +2,11 @@ package com.pokedex.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -42,6 +44,34 @@ public class PokemonController {
 	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 	private Mono<Pokemon> savePokemon(@RequestBody Pokemon pokemon) {
 		return pokedexReporitory.save(pokemon);
+	}
+	
+	@PutMapping("/{id}")
+	public Mono<ResponseEntity<Pokemon>> updatePokemon(@PathVariable String id, @RequestBody Pokemon pokemon) {
+		return pokedexReporitory.findById(id)
+				.flatMap(existingPokemon -> {
+					existingPokemon.setNome(pokemon.getNome());
+					existingPokemon.setCategoria(pokemon.getCategoria());
+					existingPokemon.setHabilidade(pokemon.getHabilidade());
+					existingPokemon.setPeso(pokemon.getPeso());
+					return pokedexReporitory.save(existingPokemon);
+				})
+				.map(updatePokemon -> ResponseEntity.ok(updatePokemon))
+				.defaultIfEmpty(ResponseEntity.notFound().build());
+	}
+	
+	@DeleteMapping("/{id}")
+	public Mono<ResponseEntity<Void>> deletePokemon(@PathVariable String id) {
+		return pokedexReporitory.findById(id)
+				.flatMap(existingPokemon -> {
+					return pokedexReporitory.delete(existingPokemon).then(Mono.just(ResponseEntity.ok().<Void>build()));
+				})
+				.defaultIfEmpty(ResponseEntity.notFound().build());
+	}
+	
+	@DeleteMapping
+	public Mono<Void> deleteAllPokemons() {
+		return pokedexReporitory.deleteAll();
 	}
 
 }
